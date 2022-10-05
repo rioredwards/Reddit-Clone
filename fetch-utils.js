321d8fd128e1f177f902c5a53b3aae3c10463ae5
Rio Edwards <rioredwards@gmail.com>
2022-10-05 13:51:30 -0700

    Add HTML/CSS for add post page

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,7 +1,9 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://kaliyzknmpknyjoukzye.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImthbGl5emtubXBrbnlqb3VrenllIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjUwMDA5MTYsImV4cCI6MTk4MDU3NjkxNn0.YGAmNYGVy_LuRt_2dyLUQblwK7G09RMqsi79XC8JglE';
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
+console.log(client);
 /* Auth related functions */
 
 export function getUser() {
