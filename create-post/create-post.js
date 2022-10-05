f1d0543010d6bf31469df58c8c5fedcb64fdc22d
Rio Edwards <rioredwards@gmail.com>
2022-10-05 14:34:38 -0700

    Image Uploads working

--- create-post/create-post.js
+++ create-post/create-post.js
@@ -1,26 +1,44 @@
 /* Imports */
 // this will check if we have a user and set signout link if it exists
 import '../auth/user.js';
-import { createPost } from '../fetch-utils.js';
+import { createPost, uploadImage } from '../fetch-utils.js';
 
 /* Get DOM Elements */
 const postForm = document.getElementById('create-post-form');
 const errorDisplay = document.getElementById('error');
 const addBtn = document.getElementById('add-post');
+const imageInput = document.getElementById('image-input');
+const preview = document.getElementById('preview');
 
 /* State */
 let error = null;
 
 /* Events */
+imageInput.addEventListener('change', () => {
+    const file = imageInput.files[0];
+    if (file) {
+        preview.src = URL.createObjectURL(file);
+    } else {
+        preview.src = '../assets/photo-placeholder.png';
+    }
+});
+
 postForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     addBtn.disabled = true;
 
     const formData = new FormData(postForm);
 
+    const imageFile = formData.get('image');
+    const randomFolder = Math.floor(Date.now() * Math.random());
+    const imagePath = `posts/${randomFolder}/${imageFile.name}`;
+
+    const url = await uploadImage('images', imagePath, imageFile);
+
     const post = {
         title: formData.get('title'),
         description: formData.get('description'),
+        image_url: url,
     };
 
     const response = await createPost(post);
