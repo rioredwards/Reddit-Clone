6720c72ac46626cef0c695a79968bca5cecc0b41
Rio Edwards <rioredwards@gmail.com>
2022-10-06 10:12:34 -0700

    Add comments on post

--- fetch-utils.js
+++ fetch-utils.js
@@ -56,6 +56,10 @@ export async function getPost(id) {
         .single();
 }
 
+export async function createComment(comment) {
+    return await client.from('comments').insert(comment).single();
+}
+
 export async function uploadImage(bucketName, imagePath, imageFile) {
     const bucket = client.storage.from(bucketName);
 
--- post/post.js
+++ post/post.js
@@ -1,12 +1,14 @@
 /* Imports */
 import '../auth/user.js';
-import { getPost } from '../fetch-utils.js';
+import { createComment, getPost } from '../fetch-utils.js';
 
 /* Get DOM Elements */
 const errorDisplay = document.getElementById('error-display');
 const postSummary = document.getElementById('post-summary');
 const postTitle = document.getElementById('post-title');
 const postDescription = document.getElementById('post-description');
+const addCommentForm = document.getElementById('add-comment-form');
+const commentList = document.getElementById('comment-list');
 
 /* State */
 let error = null;
@@ -34,6 +36,28 @@ window.addEventListener('load', async () => {
         location.assign('/');
     } else {
         displayPost();
+    }
+});
+
+addCommentForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(addCommentForm);
+    const commentInsert = {
+        post_id: post.id,
+        text: formData.get('text'),
+    };
+    const response = await createComment(commentInsert);
+
+    error = response.error;
+    const comment = response.data;
+
+    if (error) {
+        displayError();
+    } else {
+        console.log(comment);
+        post.comments.unshift(comment);
+        addCommentForm.reset();
     }
 });
 
